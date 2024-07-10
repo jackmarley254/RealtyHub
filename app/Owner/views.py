a88fae4baa46059862b2bf0c9bd29189c179a8a0
Godswill <onwusilikegodswill@gmail.com>
2024-07-10 20:51:46 +0100

    :wrench: Added routes for the property

--- app/Owner/views.py
+++ app/Owner/views.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ The views for the application """
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, Blueprint
 from app import app, db, bcrypt
 from app.models import User, Property
 from forms import RegisterForm, LoginForm
--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, DateTime, ForeignKey, String,Text, Date, func, Boolean
 from sqlalchemy.types import JSON
 from sqlalchemy.orm import relationship
+from sqlalchemy_utils import SQLAEnum
 from app import Base
 from enum import Enum
 from flask_login import UserMixin
@@ -23,7 +24,7 @@ class Owner(Base, UserMixin):
     phone = Column(String(128), nullable=False)
     image_file = Column(String(128), nullable=False, default='default.jpg')
     location = Column(String(128), nullable=False)
-    
+    property_id = relationship('Property', backref='owners', lazy=True)
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}', '{self.location}', '{self.phone}')"
 
@@ -79,30 +80,41 @@ class TenantProperty(Base):
     propertys = relationship('Property', backref='tenant_properties')
 
 
-class Property(Base):
+class PropertyStatus(Enum, UserMixin):
+    """ The property status model
+
+    Args:
+        Enum (_type_): _description_
+        UserMixin (_type_): session management
+    """
+    SALE = "sale"
+    RENT = "rent"
+    SOLD = "sold"
+    RENTED = "rented"
+
+class Property(Base, UserMixin):
     """ The property model
 
     Args:
         Base (_type_): _description_
+        UserMixin (_type_): _description_
+
+    Returns:
+        _type_: _description_
     """
-    __tablename__ = 'properties'
     id = Column(Integer, primary_key=True)
     title = Column(String(200), nullable=False)
     description = Column(Text, nullable=False)
     location = Column(String(200), nullable=False)
     price = Column(Integer, nullable=False)
     property_type = Column(String(200), nullable=False)
-    property_status = Column(String(200), nullable=False)
+    property_status = Column(SQLAEnum(PropertyStatus), nullable=False)
     bathrooms = Column(Integer, nullable=False)
     bedrooms = Column(Integer, nullable=False)
     size = Column(Integer, nullable=False)
-    # amenties = Column(JSON, nullable=False)
     available_from = Column(DateTime, nullable=False, default=func.current_timestamp())
     created_at = Column(DateTime, default=datetime.utcnow)
-    
-    # We create a one to many relationship between the user and the property
-    # A user can own many properties and a property can be owned by one user
-    owner_id = Column(Integer, ForeignKey('users.id'))
+    owner_id = Column(Integer, ForeignKey('owners.id'))
     
     def __repr__(self):
         return f"Property('{self.title}', '{self.description}', '{self.location}', '{self.price}', '{self.property_type}', '{self.property_status}', '{self.bathrooms}', '{self.bedrooms}', '{self.size}')"    
@@ -125,14 +137,3 @@ class Messages(Base, UserMixin):
     
     def __repr__(self):
         return f"Messages('{self.sender_id}', '{self.receiver_id}', '{self.property_id}', '{self.message}')"
-        
-class PropertyStatus(Enum, UserMixin):
-    """Property status enum
-
-    Args:
-        Enum (_type_): _description_
-    """
-    SALE = "sale"
-    RENT = "rent"
-    SOLD = "sold"
-    RENTED = "rented"
